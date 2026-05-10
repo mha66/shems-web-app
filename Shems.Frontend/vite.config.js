105af50055fecd5d7e295b3dcec117a72f601c4e
mha66 <mharegaily@gmail.com>
2026-05-10 23:00:21 +0300

    install sass

--- Shems.Frontend/vite.config.js
+++ Shems.Frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // This tells Sass to ignore warnings from node_modules
+        quietDeps: true 
+      }
+    }
+  }
 })
