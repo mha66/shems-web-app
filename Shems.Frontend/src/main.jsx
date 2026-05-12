add4d0e3f1638332778ddb32b0c8222e054e7363
mha66 <mharegaily@gmail.com>
2026-05-12 03:09:25 +0300

    install missing icons

--- Shems.Frontend/src/main.jsx
+++ Shems.Frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 import './index.css'
