57a3aca24e87877b5073b99251b6249546140f62
mha66 <mharegaily@gmail.com>
2026-05-12 23:44:00 +0300

    fix axios interceptor not intercepting session expired responses

--- Shems.Frontend/src/services/api.js
+++ Shems.Frontend/src/services/api.js
@@ -14,8 +14,8 @@ api.interceptors.response.use(
     // Grab the original request that just failed
     const originalRequest = error.config;
 
-    // Check if the error is a 401 (Unauthorized) AND refresh hasn't already been attempted for this request
-    if (error.response && error.response.status === 401 && !originalRequest._retry && 
+    // Check if the error is a 401 (Unauthorized) or 440 (Session Timeout) AND refresh hasn't already been attempted for this request
+    if (error.response && (error.response.status === 401 || error.response.status === 440) && !originalRequest._retry && 
       !originalRequest.url.includes('/login') && !originalRequest.url.includes('/register')) {
       
       // Mark this request as "retrying" to prevent infinite loops if the refresh also fails
